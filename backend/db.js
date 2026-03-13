64357a29c38e592365b368f11a51393a8b2d82a1
afaqxaman <afaqxamanisecom@gmail.com>
2026-03-13 05:57:34 +0500

    Add CI/CD pipline

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '@admin123#',
-  database: 'devops_erp'
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'admin123',
+  database: process.env.DB_NAME || 'devops_erp',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('DB Error:', err.message);
   } else {
     console.log('MySQL Connected!');
+    connection.release();
   }
 });
 
-module.exports = db;
+module.exports = pool;
